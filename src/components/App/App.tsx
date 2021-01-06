4ba25f4aa7cfdfeb5a2997ed54404df8f0bf78af
Scott Brabson <scottbrabson@gmail.com>
2021-01-06 12:06:18 -0700

    Add API call test and render components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import Form from "../Form/Form";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
+import { getTracksByMoodAPI } from "../../utilities/apiCalls";
 
 function App() {
+  type MoodObject = {
+    valence: number;
+    arousal: number;
+  };
+
+  const getMoodyTunes = () => {
+    console.log("clicked!");
+    getTracksByMoodAPI();
+  };
+
   return (
     <div className="App-header">
       This is the App parent component
       <Form />
+      <button onClick={getMoodyTunes}>console log API call</button>
     </div>
   );
 }

780326afd3f6d1a2653cd1a4dfce9a0fca54a105
Demaceo Howard <hdemaceo@gmail.com>
2021-01-07 11:24:25 -0700

    reposition song details

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -13,12 +13,15 @@ interface ResultProps {
 const Result = ({ key, id, artist, title, releaseDate, genre }: ResultProps) => {
     return (
         <article className="song" id={id}>
-            <button>+</button>
-            <div className="title-artist">
-            <h1>{title}</h1>
-            <h3>{artist}</h3>
-            </div>
             <div className="song-details">
+                <button className="moodBtn">+</button>
+                <div className="title-artist">
+                    <h1>{title}</h1>
+                    <h3>{artist}</h3>
+                </div>
+                <button className="favoriteBtn">*</button>
+            </div>
+            <div className="date-genre">
                 <p><b>Release Date:</b> {releaseDate}</p>
                 <p><b>Genre:</b> {genre}</p>
             </div>

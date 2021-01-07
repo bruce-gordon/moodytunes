d2356535d9cf7dac62dd4d962a640c3c2e6b33ed
Scott Brabson <scottbrabson@gmail.com>
2021-01-07 15:31:08 -0700

    Add Switch/Route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,38 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Form from "../Form/Form";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import { getTracksByMoodAPI } from "../../utilities/apiCalls";
-import ResultsView from '../ResultsView/ResultsView';
+import ResultsView from "../ResultsView/ResultsView";
 
 function App() {
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState("");
   const [songResults, setSongResults] = useState([]);
 
-  const getMoodyTunes = async(mood: string, genre: string, decade: string) => {
+  const getMoodyTunes = async (mood: string, genre: string, decade: string) => {
     console.log("clicked!");
-    const allGenres: string[] = ['rock', 'electro', 'pop', 'randb', 'hip hop', 'metal', 'classical', 'jazz', 'soundtrack', 'world', 'latin', 'reggae', 'folk', 'blues', 'country', 'vocal pop'];
-    const arousal: string = mood.split(',')[0];
-    const valence: string = mood.split(',')[1];
-    const excludedGenres: string = allGenres.filter((musicGenre) => genre !== musicGenre).join(',');
-    setSongResults(await getTracksByMoodAPI(valence, arousal, excludedGenres, decade));
+    const allGenres: string[] = [
+      "hip hop",
+      "vocal pop",
+      "rock",
+      "electro",
+      "pop",
+      "randb",
+      "metal",
+      "classical",
+      "jazz",
+      "soundtrack",
+      "world",
+      "latin",
+      "reggae",
+      "folk",
+      "blues",
+      "country"
+    ];
+    const arousal: string = mood.split(",")[0];
+    const valence: string = mood.split(",")[1];
+    const excludedGenres: string = allGenres
+      .filter(musicGenre => genre !== musicGenre)
+      .join(",");
+      console.log(excludedGenres)
+    setSongResults(
+      await getTracksByMoodAPI(valence, arousal, excludedGenres, decade)
+    );
   };
 
   return (
     <div className="App-header">
-
-      {!songResults.length && 
-      <Form
-        getMoodyTunes={getMoodyTunes}
-      />
-      }
-
-      {songResults.length &&
-      <ResultsView 
-        songResults={songResults} 
-      />
-      }
-
+      <Switch>
+        <Route
+          path='/results'
+          render={props => (<ResultsView songResults={songResults} {...props} />)}
+        />
+        <Route
+          path='/'
+          render={props => (<Form getMoodyTunes={getMoodyTunes} {...props}/>)}
+        />
+      </Switch>
     </div>
   );
 }

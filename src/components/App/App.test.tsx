e7024280a43dca03dbae968aed3a970f8992a053
Scott Brabson <scottbrabson@gmail.com>
2021-01-08 18:02:58 -0700

    Add TDD for App

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -7,3 +7,39 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import App from "./App";
+import "@testing-library/jest-dom";
+import { getTracksByMoodAPI } from "../../utilities/apiCalls";
+import { MemoryRouter, BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+jest.mock("../../utilities/apiCalls");
+
+describe("App", () => {
+  it("should without crashing", () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const heading = screen.getByText("Generate a list of songs that fit you're mood.")
+    expect(heading).toBeInTheDocument()
+  });
+
+  it("Should show me Results after submitting the Form", () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const heading = screen.getByText("Generate a list of songs that fit you're mood.")
+    expect(heading).toBeInTheDocument()
+  });
+});

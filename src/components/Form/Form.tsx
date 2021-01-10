aa6316866704f2cb78134b8efab49f7b460a3048
Demaceo Howard <hdemaceo@gmail.com>
2021-01-09 17:07:00 -0700

    Refactor form options

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -9,11 +9,12 @@ const Form = ({ getMoodyTunes }: FormProps) => {
 
   const handleClick = (event: MouseEvent) => {
     event.preventDefault();
+
     getMoodyTunes(mood, decade);
   }
 
-const getStylings = (elementId:string) => {
-  let className = decade === elementId ? 'selected-choice' : 'choice';
+const getStylings = (selector:string, elementId:string) => {
+  let className = selector === elementId ? 'selected-choice' : 'choice';
   return className 
 }
 
@@ -21,37 +22,43 @@ const getStylings = (elementId:string) => {
     <div className='form-view'>
       <form action="/action_page.php">
         <h2>Generate a list of songs that fit your mood.</h2>
-        <p className='form-subheader'>Please select your Mood:</p>
+        <br/>
+        <p className='form-subheader'>Please select your <b>Mood</b>:</p>
+        <br/>
         <div className='form-options'>
-        <input onChange={ event => setMood('660000,950000') } type="radio" id="happy" name="mood" value="happy"/>
-        <label htmlFor="happy">Happy</label><br/>
-
-        <input onChange={ event => setMood( '235000,91000') } type="radio" id="sad" name="mood" value="sad"/>
-        <label htmlFor="sad">Sad</label><br/>
-
-        <input onChange={ event => setMood( '100000,900000') } type="radio" id="content" name="mood" value="sad"/>
-        <label htmlFor="content">Content</label><br/>
-
-        <input onChange={ event => setMood( '900000,300000') } type="radio" id="enraged" name="mood" value="enraged"/>
-        <label htmlFor="enraged">Enraged</label><br/>
-
-        <input onChange={ event => setMood('895000,295000') } type="radio" id="angry" name="mood" value="angry"/>
-        <label htmlFor="angry">Angry</label>
+          <p className= { getStylings(mood, '660000,950000') } onClick={ event => setMood('660000,950000') }>
+          Happy
+          </p>
+           <p className= { getStylings(mood, '235000,91000') } onClick={ event => setMood('235000,91000') }>
+          Sad
+          </p>
+           <p className= { getStylings(mood, '100000,900000') } onClick={ event => setMood('100000,900000') }>
+          Content
+          </p>
+           <p className= { getStylings(mood, '895000,295000') } onClick={ event => setMood('895000,295000') }>
+          Angry
+          </p>
+           <p className= { getStylings(mood, '900000,300000') } onClick={ event => setMood('900000,300000') }>
+          Enraged
+          </p>
         </div>
-
-        <p className='form-subheader'>Please select a Decade:</p>
+        <br/>
+        <p className='form-subheader'>Please select a <b>Decade</b>:</p>
+        <br/>
         <div className='form-options'>
-        <p className={ getStylings('date50') } onClick={ event => setDecade('date50')}>1950's</p><br/>
-        <p className={ getStylings('date60') } onClick={ event => setDecade('date60')}>1960's</p><br/>
-        <p className={ getStylings('date70') } onClick={ event => setDecade('date70')}>1970's</p><br/>
-        <p className={ getStylings('date80') } onClick={ event => setDecade('date80')}>1980's</p><br/>
-        <p className={ getStylings('date90') } onClick={ event => setDecade('date90')}>1990's</p><br/>
-        <p className={ getStylings('date00') } onClick={ event => setDecade('date00')}>2000's</p><br/>
-        <p className={ getStylings('date10') } onClick={ event => setDecade('date10')}>2010's</p><br/>
+        <p className={ getStylings(decade, 'date50') } onClick={ event => setDecade('date50')}>1950's</p><br/>
+        <p className={ getStylings(decade, 'date60') } onClick={ event => setDecade('date60')}>1960's</p><br/>
+        <p className={ getStylings(decade, 'date70') } onClick={ event => setDecade('date70')}>1970's</p><br/>
+        <p className={ getStylings(decade, 'date80') } onClick={ event => setDecade('date80')}>1980's</p><br/>
+        <p className={ getStylings(decade, 'date90') } onClick={ event => setDecade('date90')}>1990's</p><br/>
+        <p className={ getStylings(decade, 'date00') } onClick={ event => setDecade('date00')}>2000's</p><br/>
+        <p className={ getStylings(decade, 'date10') } onClick={ event => setDecade('date10')}>2010's</p><br/>
         </div>
+        <br/>
         <button onClick={ (event: React.MouseEvent<HTMLElement>) => handleClick(event as any) }>
           <Link to='/results'>Get Songs</Link>
         </button>
+
       </form>
     </div>
    );

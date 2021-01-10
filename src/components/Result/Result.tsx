3b92887f384d6f69c103c13a17150f83ccda321f
Scott Brabson <scottbrabson@gmail.com>
2021-01-10 14:48:56 -0700

    import gsap

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,5 +1,6 @@
 import "./Result.css";
 import { ResultProps } from "../common/Types";
+import { gsap } from "gsap";
 
 const Result = ({
   id,
@@ -9,32 +10,35 @@ const Result = ({
   genre,
   addFavorite,
 }: ResultProps) => {
-
   let songTitle: string = title
     .split(" ")
     .map(word => word.charAt(0).toUpperCase() + word.substring(1))
     .join(" ");
 
-  let inFavorites: boolean = false
+  let inFavorites: boolean = false;
 
-//   const animateAddFavorite = () => {
-//     let tl = gsap.timeline() 
-//  if (!inFavorites) { 
-//    gsap.set('.favoriteBtn', {transition: 'ease 0', transform: 'rotate(0deg)'})
-//    tl.to('.favoriteBtn', .1, {translateY:3})  
-//      .to('.favoriteBtn', .3, {rotateY:360, translateY:-10}) 
-//      .to('.favoriteBtn', .3, {translateY:0})
-//      .to('.favoriteBtn', .2, {filter: 'grayscale(0%)'}, '-=.4')
-//    gsap.set('.favoriteBtn', {rotateY:0, translateY:0})
-//    inFavorites = true
-//  }
-//   }
+  const animateAddFavorite = (id:string) => {
+    let tl = gsap.timeline();
+    if (!inFavorites) {
+      gsap.set(`.${'--' + id}`, {  //start animation state
+        transition: "ease 0",
+        transform: "rotate(0deg)",
+      });
+
+      tl.to(`.${'--' + id}`, { duration: 0.1, translateY: 3 })
+        .to(`.${'--' + id}`, { duration: 0.3, rotateY: 360, translateY: -10 })
+        .to(`.${'--' + id}`, { duration: 0.3, translateY: 0 })
+        .to(`.${'--' + id}`, { duration: 0.2, filter: "grayscale(0%)" }, "-=.4");
+      gsap.set(`.${'--' + id}`, { rotateY: 0, translateY: 0 });
+
+      inFavorites = true;
+    }
+  };
 
   const handleClick = () => {
     addFavorite(id);
-    // animateAddFavorite()
+    animateAddFavorite(id);
   };
-
 
   return (
     <article className="song-result" id={id}>
@@ -44,7 +48,7 @@ const Result = ({
           <h1>{songTitle}</h1>
           <h3>{artist}</h3>
         </div>
-        <button onClick={() => handleClick()} className="favoriteBtn">
+        <button onClick={() => handleClick()} className={`favoriteBtn --${id}`}>
           ⭐
         </button>
       </div>

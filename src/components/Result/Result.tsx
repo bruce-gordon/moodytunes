9ba4b1a2071078488bd3c7ee724393ef4f9710ce
Scott Brabson <scottbrabson@gmail.com>
2021-01-10 14:22:48 -0700

    refactor ts code

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,14 +1,40 @@
 import "./Result.css";
 import { ResultProps } from "../common/Types";
 
-const Result = ({ id, artist, title, releaseDate, genre, addFavorite}: ResultProps) => {
+const Result = ({
+  id,
+  artist,
+  title,
+  releaseDate,
+  genre,
+  addFavorite,
+}: ResultProps) => {
+
+  let songTitle: string = title
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.substring(1))
+    .join(" ");
+
+  let inFavorites: boolean = false
+
+//   const animateAddFavorite = () => {
+//     let tl = gsap.timeline() 
+//  if (!inFavorites) { 
+//    gsap.set('.favoriteBtn', {transition: 'ease 0', transform: 'rotate(0deg)'})
+//    tl.to('.favoriteBtn', .1, {translateY:3})  
+//      .to('.favoriteBtn', .3, {rotateY:360, translateY:-10}) 
+//      .to('.favoriteBtn', .3, {translateY:0})
+//      .to('.favoriteBtn', .2, {filter: 'grayscale(0%)'}, '-=.4')
+//    gsap.set('.favoriteBtn', {rotateY:0, translateY:0})
+//    inFavorites = true
+//  }
+//   }
+
   const handleClick = () => {
     addFavorite(id);
+    // animateAddFavorite()
   };
-var songTitle = title;
-songTitle = title.split(' ')
-    .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
-    .join(' ');
+
 
   return (
     <article className="song-result" id={id}>

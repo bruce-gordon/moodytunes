a669952f5987f4b65b0b09cb2717f8f8745da238
Scott Brabson <scottbrabson@gmail.com>
2021-01-11 16:01:30 -0700

    merge changes

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,11 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import "./Result.css";
 import { ResultProps } from "../common/Types";
 import { gsap } from "gsap";
-import { spotify } from '../../utilities/icons';
+import { spotify } from "../../utilities/icons";
 
-const Result = (
-  {
+const Result = ({
   id,
   artist,
   title,
@@ -14,38 +13,38 @@ const Result = (
   favoriteSongs,
   addFavorite,
 }: ResultProps) => {
-  
-  const [inFavorites, setInFavorites] = useState(false)
+  const [inFavorites, setInFavorites] = useState(false);
 
   useEffect(() => {
-    checkFavoriteStatus()
+    checkFavoriteStatus();
     // updateFavoriteStatus()
-    }, [])
+  }, []);
 
   const checkFavoriteStatus = () => {
-    const match = favoriteSongs.find(song => song.id === id)
+    const match = favoriteSongs.find(song => song.id === id);
     if (match) {
-      setInFavorites(true)
-      animateAddFavorite()
+      setInFavorites(true);
+      animateAddFavorite();
     } else {
-      setInFavorites(false)
+      setInFavorites(false);
     }
-  }
+  };
 
   const animateAddFavorite = () => {
     let tl = gsap.timeline();
-      gsap.set(`.${'--' + id}`, {  //start animation state
-        transition: "ease 0",
-        transform: "rotate(0deg)",
-      });
+    gsap.set(`.${"--" + id}`, {
+      //start animation state
+      transition: "ease 0",
+      transform: "rotate(0deg)",
+    });
 
-      tl.to(`.${'--' + id}`, { duration: 0.1, translateY: 3 })
-        .to(`.${'--' + id}`, { duration: 0.3, rotateY: 360, translateY: -10 })
-        .to(`.${'--' + id}`, { duration: 0.3, translateY: 0 })
-        .to(`.${'--' + id}`, { duration: 0.2, filter: "grayscale(0%)" }, "-=.4");
-      gsap.set(`.${'--' + id}`, { rotateY: 0, translateY: 0 });
+    tl.to(`.${"--" + id}`, { duration: 0.1, translateY: 3 })
+      .to(`.${"--" + id}`, { duration: 0.3, rotateY: 360, translateY: -10 })
+      .to(`.${"--" + id}`, { duration: 0.3, translateY: 0 })
+      .to(`.${"--" + id}`, { duration: 0.2, filter: "grayscale(0%)" }, "-=.4");
+    gsap.set(`.${"--" + id}`, { rotateY: 0, translateY: 0 });
 
-      setInFavorites(true)
+    setInFavorites(true);
   };
 
   const handleClick = () => {
@@ -54,14 +53,21 @@ const Result = (
       animateAddFavorite();
     }
   };
-  
-  let songTitle = title.split(' ')
-    .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
-    .join(' ');
-  
-  let genreTitle = genre.split(' ')
-    .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
-    .join(' ');
+
+  const searchSpotify = () => {
+    let searchParams = `${title}  artist:${artist}`;
+    window.open(`https://open.spotify.com/search/${searchParams}`);
+  };
+
+  let songTitle = title
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.substring(1))
+    .join(" ");
+
+  let genreTitle = genre
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.substring(1))
+    .join(" ");
 
   return (
     <article className="song-result" id={id}>
@@ -70,20 +76,28 @@ const Result = (
           <h1>{songTitle}</h1>
           <h3>{artist}</h3>
         </div>
-        <button className="moodBtn">
-          {spotify}
-        </button>
-        <button onClick={() => handleClick()} className={`favoriteBtn --${id}`}>
-          ⭐
-        </button>
+        <div className="button-container">
+          <button onClick={() => searchSpotify()} className="spotify-button">
+            {spotify}
+          </button>
+          <button
+            onClick={() => handleClick()}
+            className={`btn favoriteBtn --${id}`}>
+            ⭐
+          </button>
+        </div>
       </div>
       <div className="date-genre">
-        <p>
-          <b>Release Date:</b> {releaseDate}
-        </p>
-        <p>
-          <b>Genre:</b> {genreTitle}
-        </p>
+        <span className='badge'>
+          <p>
+            <b>Release Date:</b> {releaseDate}
+          </p>
+        </span>
+        <span className='badge'>
+          <p>
+            <b>Genre:</b> {genreTitle}
+          </p>
+        </span>
       </div>
     </article>
   );

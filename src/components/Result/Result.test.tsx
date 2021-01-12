8e974051dcb98a2f996865d41f743a0ea2fe59ad
Scott Brabson <scottbrabson@gmail.com>
2021-01-11 18:58:18 -0700

    update and add sad path to Result tdd

--- src/components/Result/Result.test.tsx
+++ src/components/Result/Result.test.tsx
@@ -2,42 +2,70 @@ import "@testing-library/jest-dom";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Result from "./Result";
+import {fakeFavorites, testSong, testDuplicateSong} from '../common/testData'
 
 describe("Result", () => {
-  it("displays correct information in the Result", () => {
+
+  it("renders information and elements correctly in the Result", () => {
     const mockAddFavorite = jest.fn();
     render(
       <Result
-        key='123'  
-        id='1'
-        artist='some artist'
-        title='some title'
-        releaseDate='1983'
-        genre='hip hop'
+        key="123"
+        id={testSong.id}
+        artist={testSong.artist_display_name}
+        title={testSong.title}
+        releaseDate={testSong.releasedate}
+        genre={testSong.genre}
+        favoriteSongs={fakeFavorites}
         addFavorite={mockAddFavorite}
       />
     );
-    expect(screen.getByText("some artist")).toBeInTheDocument();
-    expect(screen.getByText("some title")).toBeInTheDocument();
-    expect(screen.getByText("1983")).toBeInTheDocument();
-    expect(screen.getByText("hip hop")).toBeInTheDocument();
+    const favButton = screen.getByRole('button', { name: /⭐/i })
+
+    expect(screen.getByText("John Lennon")).toBeInTheDocument();
+    expect(screen.getByText("Real Love")).toBeInTheDocument();
+    expect(screen.getByText("1988")).toBeInTheDocument();
+    expect(screen.getByText("Pop")).toBeInTheDocument();
+    expect(favButton).toBeInTheDocument();
+
   });
-  
+
   it("call addFavorite() with correct params", () => {
     const mockAddFavorite = jest.fn();
     render(
       <Result
-        key='123'  
-        id='1'
-        artist='some artist'
-        title='some title'
-        releaseDate='1983'
-        genre='hip hop'
+        key="123"
+        id={testSong.id}
+        artist={testSong.artist_display_name}
+        title={testSong.title}
+        releaseDate={testSong.releasedate}
+        genre={testSong.genre}
+        favoriteSongs={fakeFavorites}
         addFavorite={mockAddFavorite}
       />
     );
     const favButton = screen.getByRole('button', { name: /⭐/i })
     userEvent.click(favButton);
-    expect(mockAddFavorite).toHaveBeenCalledWith('1');
+    expect(mockAddFavorite).toHaveBeenCalledWith("4448");
+  });
+  
+  it("should not add duplicate songs", () => {
+    const mockAddFavorite = jest.fn();
+    render(
+      <Result
+        key="123"
+        id={testDuplicateSong.id}
+        artist={testDuplicateSong.artist_display_name}
+        title={testDuplicateSong.title}
+        releaseDate={testDuplicateSong.releasedate}
+        genre={testDuplicateSong.genre}
+        favoriteSongs={fakeFavorites}
+        addFavorite={mockAddFavorite}
+      />
+    );
+
+    const favButton = screen.getByRole('button', { name: /⭐/i })
+    userEvent.click(favButton);
+    expect(mockAddFavorite).not.toHaveBeenCalledWith();
   });
 });

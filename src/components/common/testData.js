ab814887a10d6ecc0cfc4d627f2dc6290b9f7799
Scott Brabson <scottbrabson@gmail.com>
2021-01-11 19:14:03 -0700

    update test data

--- src/components/common/testData.js
+++ src/components/common/testData.js
@@ -1,3 +1,78 @@
+export const fakeSearchResults = [
+  {
+    id: "6807",
+    title: "On Sunday afternoons in 1963",
+    artist_display_name: "Rickie Lee Jones",
+    artists: {
+      artist: {
+        mbid: "9cd9232e-7a3e-4eb9-8043-ee05c1e396ec",
+        name: "Rickie Lee Jones",
+        genre: "pop",
+      },
+    },
+    releasedate: "1979",
+    genre: "pop",
+    arousal: "134000",
+    valence: "866689",
+    popularity: "60",
+    favorite: "0",
+  },
+  {
+    id: "69928",
+    title: "Na Laetha Geal M'óige",
+    artist_display_name: "Enya",
+    artists: {
+      artist: {
+        mbid: "4967c0a1-b9f3-465e-8440-4598fd9fc33c",
+        name: "Enya",
+        genre: "vocal pop",
+      },
+    },
+    releasedate: "1988",
+    genre: "vocal pop",
+    arousal: "15000",
+    valence: "860074",
+    popularity: "60",
+    favorite: "0",
+  },
+  {
+    id: "15419",
+    title: "What I've done",
+    artist_display_name: "LINKIN PARK",
+    artists: {
+      artist: {
+        mbid: "f59c5520-5f46-4d2c-b2c4-822eabf53419",
+        name: "Linkin Park",
+        genre: "rock",
+      },
+    },
+    releasedate: "2007",
+    genre: "rock",
+    arousal: "896000",
+    valence: "315791",
+    popularity: "85",
+    favorite: "0",
+  },
+  {
+    id: "4448",
+    title: "Real love",
+    artist_display_name: "John Lennon",
+    artists: {
+      artist: {
+        mbid: "4d5447d7-c61c-4120-ba1b-d7f471d385b9",
+        name: "John Lennon",
+        genre: "pop",
+      },
+    },
+    releasedate: "1988",
+    genre: "pop",
+    arousal: "148000",
+    valence: "845690",
+    popularity: "60",
+    favorite: "0",
+  }
+];
+
 export const fakeFavorites = [
   {
     id: "6807",

15230d2a07a42f1cf074f5fe49c3959e0b0160e5
Scott Brabson <scottbrabson@gmail.com>
2021-01-11 21:51:44 -0700

    add tdd for navbar

---
+++ src/components/NavBar/NavBar.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import NavBar from "./NavBar";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter, BrowserRouter } from "react-router-dom";
+
+describe("NavBar", () => {
+  it("should render with buttons and heading", () => {
+    render(
+      <MemoryRouter>
+        <NavBar />
+      </MemoryRouter>
+    );
+
+    const navHome = screen.getByRole("link", { name: /home/i });
+    const navFav = screen.getByRole("link", { name: /favorites/i });
+    const heading = screen.getByRole("heading", { name: /moodytunes/i });
+
+    expect(heading).toBeInTheDocument();
+    expect(navHome).toBeInTheDocument();
+    expect(navFav).toBeInTheDocument();
+  });
+});

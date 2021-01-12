445448993d0634a6cca5e5d6777ae4161bb8f931
Demaceo Howard <hdemaceo@gmail.com>
2021-01-11 20:40:00 -0700

    Create NavBar component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,44 @@
+import { Route, Switch, Link } from "react-router-dom";
+import { musicNote } from '../../utilities/icons';
+import './NavBar.css'; 
+
+function NavBar() {
+    return (
+        <header className="app-header">
+        <div className="music-note" id="note-one">
+          { musicNote }
+        </div>
+         <div className="music-note" id="note-two">
+          { musicNote }
+        </div>
+        <div className="music-note" id="note-three">          
+        { musicNote }
+        </div>
+        <div className="music-note" id="note-four">          
+        { musicNote }
+        </div>
+        <div className="music-note" id="note-five">          
+        { musicNote }
+        </div>
+        <div className="music-note" id="note-six">          
+        { musicNote }
+        </div>
+        <div className="music-note" id="note-seven">          
+        { musicNote }
+        </div>
+        <Link to="/" className="app-name-link">
+            <h1 className="app-name">MoodyTunes</h1>
+          </Link>
+        <nav>
+          <Link to="/" className="nav-btn-link">
+            <div className="nav-btn">Home</div>
+          </Link>
+          <Link to='/favorites' className='nav-btn-link'>
+            <div className='nav-btn'>Favorites</div>
+          </Link>
+        </nav>
+      </header>
+    )
+}
+
+export default NavBar; 

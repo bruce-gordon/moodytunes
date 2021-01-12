213ebc76348284db0b0e7219bf389d18c4734e95
Scott Brabson <scottbrabson@gmail.com>
2021-01-11 19:14:13 -0700

    update resultsview tdd

--- src/components/ResultsView/ResultsView.test.tsx
+++ src/components/ResultsView/ResultsView.test.tsx
@@ -2,6 +2,8 @@ import "@testing-library/jest-dom";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import ResultsView from "./ResultsView";
+import {fakeSearchResults, fakeFavorites, testSong, testDuplicateSong} from '../common/testData'
+
 
 describe("ResultsView", () => {
   it("Should render Result components", () => {
@@ -9,19 +11,18 @@ describe("ResultsView", () => {
     
     render(
       <ResultsView
-        songResults={[{
-          id: '1',
-          artist_display_name: 'some artist',
-          title: 'some title',
-          releasedate: '1983',
-          genre: 'hip hop'
-        }]}
+        songResults={fakeSearchResults}
         addFavorite={mockAddFavorite}
+        moodName={ 'Content' }
+        favoriteSongs={ fakeFavorites }
       />
     );
-    expect(screen.getByText("some artist")).toBeInTheDocument();
-    expect(screen.getByText("some title")).toBeInTheDocument();
-    expect(screen.getByText("1983")).toBeInTheDocument();
-    expect(screen.getByText("hip hop")).toBeInTheDocument();
+    const mood = screen.getByRole('heading', { name: /"content" song results:/i })
+
+    expect(mood).toBeInTheDocument();
+    expect(screen.getByText("John Lennon")).toBeInTheDocument();
+    expect(screen.getByText("Real Love")).toBeInTheDocument();
+    expect(screen.getAllByText("1988")[0]).toBeInTheDocument();
+    expect(screen.getAllByText("Pop")[0]).toBeInTheDocument();
   });
-});
+}); 

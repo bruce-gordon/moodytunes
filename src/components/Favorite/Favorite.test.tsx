92646b26bad6008589d702d2bb19accd2a0215f0
Scott Brabson <scottbrabson@gmail.com>
2021-01-11 19:21:44 -0700

    update favoriteview tdd

--- src/components/Favorite/Favorite.test.tsx
+++ src/components/Favorite/Favorite.test.tsx
@@ -2,50 +2,53 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Favorite from "./Favorite";
+import { fakeFavorites, testSong } from "../common/testData";
 
 describe("Favorite", () => {
+  const mockRemoveFavorite = jest.fn();
+
   it("displays correct information in the Favorite", () => {
-    const mockRemoveFavorite = jest.fn();
     render(
       <Favorite
         key="123"
-        id="1"
-        artist="some artist"
-        title="some title"
-        releaseDate="1983"
-        genre="hip hop"
+        id={testSong.id}
+        artist={testSong.artist_display_name}
+        title={testSong.title}
+        releaseDate={testSong.releasedate}
+        genre={testSong.genre}
+        favoriteSongs={fakeFavorites}
         removeFavorite={mockRemoveFavorite}
       />
     );
-    const artist = screen.getByText("some artist");
-    const title = screen.getByText("some title");
-    const releaseDate = screen.getByText("1983");
-    const genre = screen.getByText("hip hop");
     const removeBtn = screen.getByRole("button", { name: /remove/i });
 
-    expect(artist).toBeInTheDocument();
-    expect(title).toBeInTheDocument();
-    expect(releaseDate).toBeInTheDocument();
-    expect(genre).toBeInTheDocument();
+    expect(screen.getByText("John Lennon")).toBeInTheDocument();
+    expect(screen.getByText("Real Love")).toBeInTheDocument();
+    expect(screen.getByText("1988")).toBeInTheDocument();
+    expect(screen.getByText("Pop")).toBeInTheDocument();
     expect(removeBtn).toBeInTheDocument();
   });
 
   it("call removeFavorite() with correct params", () => {
     const mockRemoveFavorite = jest.fn();
+
     render(
       <Favorite
         key="123"
-        id="1"
-        artist="some artist"
-        title="some title"
-        releaseDate="1983"
-        genre="hip hop"
+        id={testSong.id}
+        artist={testSong.artist_display_name}
+        title={testSong.title}
+        releaseDate={testSong.releasedate}
+        genre={testSong.genre}
+        favoriteSongs={fakeFavorites}
         removeFavorite={mockRemoveFavorite}
       />
     );
+
     const removeBtn = screen.getByRole("button", { name: /remove/i });
-    expect(removeBtn).toBeInTheDocument();
+
     userEvent.click(removeBtn);
-    expect(mockRemoveFavorite).toHaveBeenCalledWith('1');
+
+    expect(mockRemoveFavorite).toHaveBeenCalledWith("4448");
   });
 });
